de50bc3fc31343a8adbc4f501c45c3790d1f5aeb
Jonathan Christ <matthewc.calaycay@gmail.com>
2024-04-15 08:49:40 +0800

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PinchToZoom
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace PinchToZoom
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Reset size {count} time";
+            else
+                CounterBtn.Text = $"Reset size {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+            Zoomer.ScaleTo(1, 1);
+        }
+
+        private void Zoom(object sender, EventArgs e)
+        {
+            Image img = (Image)sender;
+            img.ScaleTo(img.Scale*2, 100);
+        }
+    }
+
+}
